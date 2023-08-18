a1f6655384092b24ed9ac2571f33497950445d3c
MiltonCarreno <miltonyaredcr@gmail.com>
2023-08-17 21:15:16 -0700

    Added 'get_ant_coordinates' fn to obtain and parse initial ant coordinates

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,33 @@ fn get_board_size() -> usize {
     return input_size.trim().parse().unwrap();
 }
 
+fn get_ant_coordinates() -> (usize, usize) {
+    // Prompt user for ant coordinates
+    print!("\nProvide coordinates separated by 'space' (e.g '5 3'): ");
+    match io::stdout().flush() {
+        Ok(_) => (),
+        Err(err) => println!("Buffer Flushing Error: {}", err)
+    }
+    // Get ant coordinates
+    let mut input_coordinates = String::new();
+    io::stdin()
+    .read_line(&mut input_coordinates)
+    .expect("Failed to read line");
+    // Parse and return ant coordinates
+    let vec_coordinates: Vec<_> = input_coordinates
+    .trim()
+    .split_whitespace()
+    .collect();
+    return (
+        vec_coordinates[0].parse::<usize>().unwrap(),
+        vec_coordinates[1].parse::<usize>().unwrap()
+    );
+}
+
 fn main() {
     // Obtain board size and ant's coordinates
     let size = get_board_size();
+    let coordinates = get_ant_coordinates();
+
+    println!("Size: {}, Coordinates: {} {}", size, coordinates.0, coordinates.1);
 }
