f5a4aad3cbef707f799898a21a3cba0e133a21ae
MiltonCarreno <miltonyaredcr@gmail.com>
2023-08-17 21:12:25 -0700

    Added 'get_board_size' fn to obtain and parse board size

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::io::{self, Write};
+
+struct LangtonsAnt {
+    board: Vec<Vec<char>>,
+    size: usize,
+    coordinates: (usize, usize)
+}
+
+fn get_board_size() -> usize {
+    // Prompt user for board size
+    print!("\nProvide board size: ");
+    match io::stdout().flush() {
+        Ok(_) => (),
+        Err(err) => println!("Buffer Flushing Error: {}", err)
+    }
+    // Get board size
+    let mut input_size = String::new();
+    io::stdin()
+        .read_line(&mut input_size)
+        .expect("Failed to read line");
+    // Parse and return board size
+    return input_size.trim().parse().unwrap();
+}
+
 fn main() {
-    
+    // Obtain board size and ant's coordinates
+    let size = get_board_size();
 }
