92b3a9830139fd03568a979d2520a7a28a921f5f
MiltonCarreno <miltonyaredcr@gmail.com>
2023-08-17 22:15:23 -0700

    Added 'print_board' fn to 'LangtonsAnt' struct and create a 'new' fn from 'create_board' fn

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,31 @@ struct LangtonsAnt {
     coordinates: (usize, usize)
 }
 
+impl LangtonsAnt {
+    fn new(size: usize, coordinates: (usize, usize)) -> LangtonsAnt {
+        println!("\nCreating Board!");
+        let mut board: Vec<Vec<char>> = Vec::new();
+        for row in 0..size {
+            let mut v: Vec<char> = vec![' '; size as usize];
+            if row == coordinates.0 {
+                v[coordinates.1] = '&';
+            }
+            board.push(v);
+        }
+        return LangtonsAnt { board, size, coordinates };
+    }
+
+    fn print_board(&self) {
+        println!("\nPrinting Board!");
+        let border: Vec<char> = vec!['-'; self.size];
+        println!(" {}", border.iter().collect::<String>());
+        for row in &self.board {
+            println!("|{}|", row.iter().collect::<String>());
+        }
+        println!(" {}", border.iter().collect::<String>());
+    }
+}
+
 fn get_board_size() -> usize {
     // Prompt user for board size
     print!("\nProvide board size: ");
@@ -49,40 +74,13 @@ fn main() {
     // Obtain board size and ant's coordinates
     let size = get_board_size();
     let coordinates = get_ant_coordinates();
-    let board = create_board(size, coordinates);
 
     println!("\n--User input--\nBoard Size: {}\nCoordinates: {} {}",
     size, coordinates.0, coordinates.1);
 
     // Construct Langton's Ant
-    let mut ant = LangtonsAnt {
-        board,
-        size,
-        coordinates
-    };
+    let mut ant = LangtonsAnt::new(size, coordinates);
 
-    print_board(&ant);
-}
-
-fn create_board(size: usize, coordinates: (usize, usize)) -> Vec<Vec<char>> {
-    println!("\nCreating Board!");
-    let mut board: Vec<Vec<char>> = Vec::new();
-    for row in 0..size {
-        let mut v: Vec<char> = vec![' '; size as usize];
-        if row == coordinates.0 {
-            v[coordinates.1] = '&';
-        }
-        board.push(v);
-    }
-    return board;
-}
-
-fn print_board(ant: &LangtonsAnt) {
-    println!("\nPrinting Board!");
-    let border: Vec<char> = vec!['-'; ant.size];
-    println!(" {}", border.iter().collect::<String>());
-    for row in &ant.board {
-        println!("|{}|", row.iter().collect::<String>());
-    }
-    println!(" {}", border.iter().collect::<String>());
+    // Print board
+    ant.print_board();
 }
